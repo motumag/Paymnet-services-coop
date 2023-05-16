2c13e0e4c3dcee4eb129b4ea97d8965883a2362a
motumag <motumagishu27@gmail.com>
2023-05-16 11:25:41 +0300

    contact person is added

--- src/main/java/com/dxvalley/nedajpaymnetbackend/PaymentServicesApplication.java
+++ src/main/java/com/dxvalley/nedajpaymnetbackend/PaymentServicesApplication.java
@@ -1,7 +1,9 @@
 package com.dxvalley.nedajpaymnetbackend;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -11,7 +13,16 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
         info = @Info(
                 title = "Payment Service",
                 version = "1.0",
-                description = "Payment Service Backend @CoopBank"
+                description = "Payment Service Backend @CoopBank",
+                contact = @Contact(
+                        name = "Motuma Gishu",
+                        email = "motumag@coopbankoromia.com.et",
+                        url = "https://www.coopbankoromia.com"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://www.apache.org/licenses/LICENSE-2.0"
+                )
         )
 )
 public class PaymentServicesApplication {
