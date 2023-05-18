9be02c4c85fd1fc0b0146bd7baa4d5b359202d1d
motumag <motumagishu27@gmail.com>
2023-05-18 15:56:29 +0300

    api access by token

--- src/main/java/com/dxvalley/nedajpaymnetbackend/security/config/SecurityConfiguration.java
+++ src/main/java/com/dxvalley/nedajpaymnetbackend/security/config/SecurityConfiguration.java
@@ -24,7 +24,7 @@ public class SecurityConfiguration {
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/api/v1/auth/**","/api/docs/")
+                .requestMatchers("/api/v1/auth/**","/api/docs/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
