75b6bc9c18739a4bca781752f3827ef753a2f136
motumag <motumagishu27@gmail.com>
2023-05-18 18:20:49 +0300

    added doc

--- src/main/java/com/dxvalley/nedajpaymnetbackend/security/config/JwtAuthenticationFilter.java
+++ src/main/java/com/dxvalley/nedajpaymnetbackend/security/config/JwtAuthenticationFilter.java
@@ -39,13 +39,14 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
         String path = request.getRequestURI();
         if (path.contains("/api/v1/auth/register")
-                || path.contains("/api/v1/auth/authenticate")
-                ||path.contains("/api/docs/")
-                ||path.contains("/swagger-ui.html")
-                ||path.contains("/swagger-ui/")
-                ||path.contains("/v3/api-docs/")) {
+                || path.contains("/api/v1/auth/authenticate")) {
             filterChain.doFilter(request, response);
             return;
+
+//            ||path.contains("/api/docs/")
+//                    ||path.contains("/swagger-ui.html")
+//                    ||path.contains("/swagger-ui/")
+//                    ||path.contains("/v3/api-docs/")
         }
         if(authHeader==null ||!authHeader.startsWith("Bearer ")){
             handleMissingTokenException(response);
