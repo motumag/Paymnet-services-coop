ce5723d787e9f7486e59a99e52040cc9dd9fbc0f
motumag <motumagishu27@gmail.com>
2023-05-18 18:33:30 +0300

    the token security

--- src/main/java/com/dxvalley/nedajpaymnetbackend/PaymentServicesApplication.java
+++ src/main/java/com/dxvalley/nedajpaymnetbackend/PaymentServicesApplication.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -23,7 +24,10 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
                         name = "Apache 2.0",
                         url = "https://www.apache.org/licenses/LICENSE-2.0"
                 )
-        )
+        ),
+        security = {
+                @SecurityRequirement(name = "bearerToken")
+        }
 )
 public class PaymentServicesApplication {
 
